93e7045da1f213cf7cd844a418b0ccb295bb6f7b
evad-e <cd.empinado@gmail.com>
2024-07-24 15:29:37 +0800

    feat: Add seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -14,10 +14,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $accounts = [
+          ['name' => 'Josephine Cacao', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Leonilo Cacao', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Fernando Giba Jr', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Chenny Cao', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Merry', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Ken Somirachi', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Nicka Mendoza', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Rosheen Perdillo', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Jason Prado', 'created_at'=>now(), 'updated_at'=>now()],
+          ['name' => 'Joan Berlin', 'created_at'=>now(), 'updated_at'=>now()],
+        ];
+        DB::table('accounts')->insert($accounts);
     }
 }
